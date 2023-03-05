8c55bcc9cd09fb01c08a07905a0cffca699e9963
Konstantin Shilyaev <konstantin.shilyaev@playrix.com>
2023-03-05 12:15:05 +0200

    Рассылка за 1 день, а не за 0

--- bot/settings.py
+++ bot/settings.py
@@ -16,9 +16,9 @@ class Settings(BaseSettings):
     roug_calc_step: int = 3 * 60 * 60
     exact_calc_step: int = 120
     # За сколько дней начать оповещения
-    # 0: за 1 день
-    # 1: за 2 дня и т.д.
-    notification_days: int = 0
+    # 1: за 1 день
+    # 2: за 2 дня и т.д.
+    notification_days: int = 1
 
 
 load_dotenv()
